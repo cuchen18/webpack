b1d235e734d039c2b5c841cda9ad8cb4978666ff
cuchen18 <861466313@qq.com>
2022-06-17 21:51:52 +0800

    完成里js高级、图片处理

--- src/main.js
+++ src/main.js
@@ -1,2 +1,12 @@
 // import '../public/index.html'
 import './style/index.css'
+import './style/index.less'
+import './assets/fonts/iconfont.css'
+import jpgs from './assets/1.gif'
+const jpg=document.createElement('img')
+jpg.src=jpgs
+document.body.appendChild(jpg)
+const nums=()=>{
+    console.log('ok');
+}
+console.log(nums);
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,50 @@
-const path=require('path')
+const path = require("path");
 // 引入html的插件
-const HtmlWebpackPlugin=require('html-webpack-plugin')
-module.exports={
-    mode:'development',
-    entry:'./src/main.js',
-    output:{
-        path:path.join(__dirname,'lib'),
-        filename:'webpack-index.js',
-        clean:true
-    },
-    plugins:[
-        new HtmlWebpackPlugin({
-            template:'./public/index.html',
-            filename:'index.html'
-        })
-    ],
-    module:{
-        rules:[
-            {
-                test:/\.css$/i,
-                use:["style-loader","css-loader"]
-            }
-        ]
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  mode: "development",
+  entry: "./src/main.js",
+  output: {
+    path: path.join(__dirname, "lib"),
+    filename: "webpack-index.js",
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.less$/i,
+        use: ["style-loader", "css-loader", "less-loader"],
+      },
+      { test: /\.(png|gif|jpeg|jpg)$/i,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 2 * 1024,
+          },
+        },
+        generator:{
+            filename:'fonts/[hash:6][ext]'
+          }
+      },
+    {
+        test:/\.js$/i,
+        use:["babel-loader"]
     }
-}
+    ],
+  },
+  devServer: {
+    port: 30000,
+    open: true,
+  },
+
+};
